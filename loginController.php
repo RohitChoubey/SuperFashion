7af5c0ac4a5ab193e5ff970cd76215397a2506de
Rohit Choubey <41469783+RohitChoubey@users.noreply.github.com>
2021-03-22 15:52:40 +0530

    add jQuery

--- loginController.php
+++ loginController.php
@@ -1,33 +1,47 @@
 <?php
 session_start();
 require("dbconfig.php");
-$username = $_POST['username'];
-$pass = $_POST['pass'];
-
-
-if ($_SERVER["REQUEST_METHOD"]== "POST"){
-    $sql = mysqli_query($db,"select * from user where username='$username' AND pass='$pass'");
-   if(mysqli_num_rows($sql)==1){
-	   $qry = mysqli_fetch_array($sql);
-        $_SESSION['username'] = $qry['username'];
-		$_SESSION["id"] = $qry['id'];
-		$_SESSION["login"] = "OK";
-		?>
-        <script language="JavaScript">
-            alert('You Login SucessFully!');
-            document.location='AddProduct.php';
-        </script>
-        <?php
-    }
-	else{
-        ?>
-        <script language="JavaScript">
-            alert('Enter Correct User Name and pass!');
-            document.location='login/';
-        </script>
-        <?php
-    }
-}else {
-    header("location:index.php");
+if(!($_SERVER["REQUEST_METHOD"] == "POST" )){
+    header('Location:index.php');
+    exit();
 }
+    $data = stripslashes(file_get_contents("php://input"));
+    $mydata = json_decode($data, true);
+   /* $id = $mydata['data'];*/
+    $username = $mydata['username'];
+    $pass = $mydata['pass'];
+    /*$password = $mydata['password'];
+    $username = $_POST['username'];
+    $pass = $_POST['pass'];
+  ``print_r($username);
+      print_r($pass);
+    exit();
+*/
+
+
+
+
+    if(!empty($username) && !empty($pass) ){
+       $sql = mysqli_query($db,"select * from user where username='$username' AND pass='$pass'");
+       if(mysqli_num_rows($sql)==1){
+    	   $qry = mysqli_fetch_array($sql);
+            $_SESSION['username'] = $qry['username'];
+    		$_SESSION["id"] = $qry['id'];
+    		$_SESSION["login"] = "OK";
+    		?>
+            <script language="JavaScript">
+                alert('You Login SucessFully!');
+                document.location='../AddProduct.php';
+            </script>
+            <?php
+        }
+    	else{
+           $msg = "Please Enter Correct Username and password.";
+         echo $msg;
+        }
+    }
+    else {
+         $msg = "Please Enter Username and password.";
+         echo $msg;
+    }
 ?>
