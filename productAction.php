f2a222695866f48d73e62b6539c24e27db187f72
Rohit Choubey <41469783+RohitChoubey@users.noreply.github.com>
2021-03-22 15:54:18 +0530

    update

--- productAction.php
+++ productAction.php
@@ -1,18 +1,58 @@
 <?php
-session_start();
-if(!($_SERVER["REQUEST_METHOD"] == "POST" )){
-    header('Location:index.php');
-    exit();
-}
-require_once 'dbConfig.php'; 
+	session_start();
+	if(!(isset($_SESSION["login"]) && $_SESSION["login"] == "OK")) {
+		header("Location: index.php");
+		exit();
+	}
+	//$errors = array(); 
 
-if (count($_POST) > 0) {
-    $result = mysqli_query($db, "SELECT *from user");
-    $row = mysqli_fetch_array($result);
-    if ($_POST["currentPassword"] == $row["pass"]) {
-        mysqli_query($db, "UPDATE user set pass='" . $_POST["newPassword"] . "'");
-        $message = "Password Change Successfully";
-    } else
-        $message = "Current Password is not correct";
-}
+	// connect to the database
+	require ("dbconfig.php");
+	
+	// File upload path
+	$targetDir = "uploads/";
+
+// initializing variables
+	$category = "";
+	$image ="";
+  // receive all input values from the form
+  
+	$fileName = basename($_FILES["image"]["name"]); 
+	$targetFilePath = $targetDir . $fileName;
+	$fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
+	$category = mysqli_real_escape_string($db, $_POST['category']);
+	
+	if (empty($category)) { 
+		array_push($errors, "Category Name is required"); 
+	}
+    $allowTypes = array('jpg','png','jpeg','gif','pdf','.JPG');
+	//if(in_array($fileType, $allowTypes)){
+		  // Upload file to server
+        if(move_uploaded_file($_FILES["image"]["tmp_name"], $targetFilePath)){
+	
+		/* $image = $_FILES['image']['tmp_name']; 
+		$imgContent = addslashes(file_get_contents($image)); */
+		
+		//$query = "insert into product(category, img,time)values('$category', '".$fileName."',NOW())";
+		 $insert = $db->query("insert into product (category, img, time) VALUES ('$category','$fileName', NOW())");
+		//mysqli_query($con,$query);
+		if($insert){
+			$statusMsg = "The file ".$fileName. " has been uploaded successfully.";
+			header("Location: AddProduct.php"); 
+		}else{
+			$statusMsg = "File upload failed, please try again.";
+			} 
+		}else{
+			$statusMsg = "Sorry, there was an error uploading your file.";
+		}
+	/* }
+	else{
+        $statusMsg = 'Sorry, only JPG, JPEG, PNG, GIF, & PDF files are allowed to upload.';
+    } */
+	// Display status message
+echo $statusMsg;
+	/* $_SESSION['success'] = "Your Product is Added Sucessfully"; 
+	header("Location: AddProduct.php"); 
+    echo "insert sucessfully"; */
+
 ?>
